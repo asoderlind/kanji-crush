b1f4e9f8aa2112606ca8ec87bb957dfc794cc511
Axel Söderlind <axesode@gmail.com>
2021-01-02 22:00:30 +0100

    fixed completed board but unable to progress on coming back from pause + minor fixes

--- app/src/main/java/com/example/kanjicrush2/MainActivity.java
+++ app/src/main/java/com/example/kanjicrush2/MainActivity.java
@@ -23,23 +23,19 @@ import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-    // Declare object for the gridView
     private GridView mGridView;
     public static final String msg = "Android : ";
 
     // Game-specific variables
     private static final int[][] mLevelDimensions = {{3,4},{3,6},{6,4},{6,5},{6,6}};
-    private static int ROWS;
-    private static int COLUMNS;
-    private static int DIMENSIONS;
+    private static final int mNumWordsStart = 4;
 
-    // non-constant variables
+    // Board dimensions
+    private static int ROWS, COLUMNS, DIMENSIONS;
     private static int mColumnWidth, mColumnHeight;
-    private static int mLevel = 0;
-    private static int mNumWords = 4;
 
-    // Declaration the local lists
+    // level-specific variables
+    private static int mLevel = 0;
     private static String mJukus;
     private static String mKanjis;
     private static int[] mButtonStateList;
@@ -50,50 +46,45 @@ public class MainActivity extends AppCompatActivity {
     /** Calls all the methods needed to initialize a new level */
     private void advanceLevel(){
         Log.d(msg, "advanceLevel() called");
-        if (mLevel < 4){
-            mLevel++;
-        } else {
-            // reset if limit reached
-            mLevel = 0;
-        }
-        mNumWords = 4 + mLevel*2;
-        setRowsAndColumns();
-        initLists();
-        initView();
-        initText();
+
+        // increase level if below level 5, else reset to 0
+        mLevel = (mLevel < 4) ? mLevel + 1 : 0;
+
+        // Initialize new variables
+        mJukus = getJukus(4 + mLevel*2);
+        mKanjis = getScrambled(mJukus);
+        ROWS = mLevelDimensions[mLevel][0];
+        COLUMNS = mLevelDimensions[mLevel][1];
+        DIMENSIONS = ROWS*COLUMNS;
+        mButtonStateList = new int[DIMENSIONS];
+        Log.d(msg, "New Jukus: " + mJukus);
+        Log.d(msg, "New scrambled kanjis: " + mKanjis);
+
+        // Update the level
+        initView(); // New number of columns
+        initLevelText(); // New level text
         initNextLevelButton();
         setDimensions();
     }
 
-    /** Checks if a line is completed and if so calls checkCompleteBoard,
+    /** Checks if a line of 3 kanjis is completed and if so calls checkCompleteBoard,
      * otherwise calls updateButtonBackgrounds in order to deselect buttons. */
-    private void checkForCompletedLine(){
-        Log.d(msg,"checkForCompletedLine() called");
+    private void checkForCompletedJukugo(){
+        Log.d(msg,"checkForCompletedJukugo() called");
         boolean lineCompleted = false;
         for (int i = 0; i < mKanjis.length()/3; i++){
-            String candidate;
             char char1;
             char char2;
             char char3;
-
-            if (i >= COLUMNS){
-                //Log.d(msg,"Second row, candidate: ");
-                char1 = mKanjis.charAt(i + (2 * COLUMNS));
-                char2 = mKanjis.charAt(i + (3 * COLUMNS));
-                char3 = mKanjis.charAt(i + (4 * COLUMNS));
-            } else {
-                //Log.d(msg,"First row, candidate: ");
-                char1 = mKanjis.charAt(i);
-                char2 = mKanjis.charAt(i + COLUMNS);
-                char3 = mKanjis.charAt(i + 2 * COLUMNS);
-            }
-
-            candidate = String.valueOf(char1) + char2 + char3;
-
+            // If (i >= COLUMNS) it is the first row, otherwise it is the second row of we are comparing on
+            char1 = (i >= COLUMNS) ? mKanjis.charAt(i + (2 * COLUMNS)) : mKanjis.charAt(i);
+            char2 = (i >= COLUMNS) ? mKanjis.charAt(i + (3 * COLUMNS)) : mKanjis.charAt(i + COLUMNS);
+            char3 = (i >= COLUMNS) ? mKanjis.charAt(i + (4 * COLUMNS)) : mKanjis.charAt(i + 2 * COLUMNS);
+            String candidate = String.valueOf(char1) + char2 + char3;
             //Log.d(msg,candidate);
+
             for (int j=0; j < mJukus.length(); j += 3) {
-                String juku = mJukus.substring(j, j + 3); //end index exclusive
-                //Log.d(msg,"Comparing to: " + juku);
+                String juku = mJukus.substring(j, j + 3);
                 if (candidate.equals(juku)) {
                     lineCompleted = true;
                     if (i >= COLUMNS) {
@@ -107,10 +98,10 @@ public class MainActivity extends AppCompatActivity {
                     }
                     Log.d(msg,"Line completed, calling checkForCompletedBoard");
                     checkForCompleteBoard();
+
                 }
             }
         }
-
         if(!lineCompleted){
             Log.d(msg,"no line completed, deselect buttons, calling UBG");
             updateButtonBackgrounds();
@@ -122,14 +113,15 @@ public class MainActivity extends AppCompatActivity {
     private void checkForCompleteBoard() {
         Log.d(msg,"checkForCompletedBoard() called");
         int numCompleted = 0;
+
         for (int i = 0; i < DIMENSIONS; i++) {
             if (mButtonStateList[i] == 2) numCompleted++;
         }
+
         if (numCompleted == DIMENSIONS && !mSwapAnimationRunning){
-            //advanceLevel();
             Button nextLevelButton = findViewById(R.id.btnSubmit);
             if (mLevel >= 5) {
-                nextLevelButton.setText(R.string.lower_button_text);
+                nextLevelButton.setText(R.string.reset);
             }
             updateButtonBackgrounds();
             nextLevelButton.setEnabled(true);
@@ -192,6 +184,7 @@ public class MainActivity extends AppCompatActivity {
             button.setText(String.valueOf(mKanjis.charAt(button.getId())));
             button.setTextColor(Color.parseColor("#FFFFFFFF"));
             button.setTextSize(35);
+            button.setOnClickListener(myOnClickListener(i));
 
             //Setting initial bg in case of a loaded game
             if(mButtonStateList[i] == 0) {
@@ -205,7 +198,6 @@ public class MainActivity extends AppCompatActivity {
                 button.setEnabled(false);
             }
 
-            button.setOnClickListener(myOnClickListener(i));
             buttons.add(button);
         }
         return buttons;
@@ -270,22 +262,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /** Create and assign the various lists */
-    private void initLists() {
-        Log.d(msg, "initLists() called");
-
-        mJukus = getJukus(mNumWords);
-        mKanjis = getScrambled(mJukus);
-        Log.d(msg, "Jukus: " + mJukus + "Scrambled kanjis: " + mKanjis);
-
-        mButtonStateList = new int[DIMENSIONS];
-        for( int i=0; i < DIMENSIONS; i++) {
-            mButtonStateList[i] = 0;
-        }
-    }
-
     /** Add text to the textView at the top */
-    private void initText() {
+    private void initLevelText() {
         TextView levelTextView = findViewById(R.id.levelText);
         String levelAnnotation = "Level: " + (mLevel + 1) + "/5";
         levelTextView.setTextColor(Color.parseColor("#FFFFFFFF"));
@@ -299,32 +277,30 @@ public class MainActivity extends AppCompatActivity {
         mGridView.setNumColumns(COLUMNS);
     }
 
-    private void loadSharedPreferences(){
+    /** Assign loaded data and return true if loaded successfully */
+    private boolean loadSharedPreferences(){
         Log.d(msg, "loadSharedPreferences() called");
-        // Load shared prefs
         SharedPreferences sharedPref = getSharedPreferences("mySettings", MODE_PRIVATE);
-
-        // load the level
         mLevel = sharedPref.getInt("myLevel", 0);
-        Log.d(msg, "The myLevel variable is: " + mLevel);
-
-        // load the jukus and kanjis
         mJukus = sharedPref.getString("myJukus", null);
         mKanjis = sharedPref.getString("myKanjis", null);
-        Log.d(msg, "The myJukus variable is: " + mJukus);
-        Log.d(msg, "The myKanjis variable is: " + mKanjis);
-
-        // load the states
         String myStates = sharedPref.getString("myStates", null);
-        Log.d(msg, "The myStates variable is: " + myStates);
+        ROWS = mLevelDimensions[mLevel][0];
+        COLUMNS = mLevelDimensions[mLevel][1];
+        DIMENSIONS = ROWS*COLUMNS;
+
+        Log.d(msg, "Loaded myLevel: " + mLevel);
+        Log.d(msg, "Loaded myJukus: " + mJukus);
+        Log.d(msg, "Loaded myKanjis: " + mKanjis);
+        Log.d(msg, "Loaded myStates:" + myStates);
+
         if (myStates != null){
-            Log.d(msg, "myStates is not null");
             mButtonStateList = new int[myStates.length()];
-            for(int i=0; i < myStates.length(); i++) {
+            for(int i = 0; i < myStates.length(); i++) {
                 mButtonStateList[i] = Character.getNumericValue(myStates.charAt(i));
             }
-            logStateList();
         }
+        return ((mJukus != null) && (mKanjis != null) && (myStates != null));
     }
 
     /** Print the state list to log*/
@@ -343,15 +319,13 @@ public class MainActivity extends AppCompatActivity {
             public void onAnimationStart(Animation animation) {
 
             }
-
             @Override
             public void onAnimationEnd(Animation animation) {
                 // Calling for a second time, after animation has ended, to maybe load the next level
                 Log.d(msg, "Calling checkForCompletedLine() from onAnimationEnd()");
                 mSwapAnimationRunning = false;
-                checkForCompletedLine();
+                checkForCompletedJukugo();
             }
-
             @Override
             public void onAnimationRepeat(Animation animation) {
 
@@ -380,22 +354,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loadSharedPreferences();
-        setRowsAndColumns();
-        if (mJukus == null){
-            initLists();
+        if (!loadSharedPreferences()){
+            mJukus = getJukus(mNumWordsStart);
+            mKanjis = getScrambled(mJukus);
+            ROWS = mLevelDimensions[mLevel][0];
+            COLUMNS = mLevelDimensions[mLevel][1];
+            DIMENSIONS = ROWS*COLUMNS;
+            mButtonStateList = new int[DIMENSIONS];
+            Log.d(msg, "New Jukus: " + mJukus);
+            Log.d(msg, "New Scrambled kanjis: " + mKanjis);
+            logStateList();
         }
-        initText();
+        initLevelText();
         initNextLevelButton();
         initView();
-        setDimensions(); // set COLUMNS, ROWS, DIMENSIONS, mColumnWidth and mColumnHeight
+        setDimensions();
     }
 
     /** Called just before the activity is destroyed. */
     @Override
     public void onDestroy() {
         super.onDestroy();
-        saveSharedPreferences();
         Log.d(msg, "The onDestroy() event");
     }
 
@@ -411,6 +390,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.d(msg, "The onResume() event");
+        if(mButtonStateList != null){
+            checkForCompleteBoard();
+        }
     }
 
     /** Called when the activity is about to become visible. */
@@ -437,27 +419,19 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sharedPref = getSharedPreferences("mySettings", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
 
-        // Add the current level
+        // Add the variables
         editor.putInt("myLevel", mLevel);
-        Log.d(msg, "The myLevel variable is: " + mLevel);
-
-        // Add the juku list as single string
         editor.putString("myJukus", mJukus);
-        Log.d(msg, "The mJukus variable is: " + mJukus);
-
-        // Add the kanji list as single string
         editor.putString("myKanjis", mKanjis);
-        Log.d(msg, "The mKanjis variable is: " + mKanjis);
-
-        // Add the state list as single string
         StringBuilder myStates = new StringBuilder();
-        for (int value : mButtonStateList) {
-            myStates.append(value);
-        }
+        for (int value : mButtonStateList) { myStates.append(value); }
         editor.putString("myStates", myStates.toString());
-        Log.d(msg, "The myStates variable is: " + myStates);
 
-        // Apply changes
+        /*Log.d(msg, "The myLevel variable is: " + mLevel);
+        Log.d(msg, "The mJukus variable is: " + mJukus);
+        Log.d(msg, "The mKanjis variable is: " + mKanjis);
+        Log.d(msg, "The myStates variable is: " + myStates);*/
+
         editor.apply();
     }
 
@@ -472,29 +446,15 @@ public class MainActivity extends AppCompatActivity {
                 mGridView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
                 int displayWidth = mGridView.getMeasuredWidth() - 100; // -140 to account for style margins
                 int displayHeight = mGridView.getMeasuredHeight();
-
                 int statusbarHeight = getStatusBarHeight(getApplicationContext());
                 int requiredHeight = displayHeight - statusbarHeight;
-
                 mColumnWidth = displayWidth / COLUMNS;
-
-                if (ROWS/3 > 1) {
-                    mColumnHeight = requiredHeight / (ROWS + 1); // +1 for a bit of extra space
-                } else {
-                    mColumnHeight = requiredHeight / ROWS; // +1 for a bit of extra space
-                }
+                mColumnHeight = (mLevel > 1) ? requiredHeight / (ROWS + 1) : requiredHeight / ROWS;
 
                 Log.d(msg, "Column dimensions: " + (mColumnWidth) + "x" + (mColumnHeight));
                 display(getApplicationContext());
             }
         });
-    }
-
-    private void setRowsAndColumns() {
-        Log.d(msg, "setRowsAndColumns() called");
-        ROWS = mLevelDimensions[mLevel][0];
-        COLUMNS = mLevelDimensions[mLevel][1];
-        DIMENSIONS = ROWS*COLUMNS;
     }
 
     private void swapChips(){
@@ -545,8 +505,7 @@ public class MainActivity extends AppCompatActivity {
                                 button2.setText(String.valueOf(mKanjis.charAt(l)));
 
                                 // Calling for the first time, to prepare backgrounds of buttons before cancellation of animation
-                                Log.d(msg, "checkForCompletedLine() from run()");
-                                checkForCompletedLine();
+                                checkForCompletedJukugo();
 
                                 // Cancel animation to avoid potential flickering @ end of animation
                                 animation1.cancel();
@@ -562,24 +521,20 @@ public class MainActivity extends AppCompatActivity {
     // TODO: make more elegant solution to try-catch blocks
     private void updateButtonBackgrounds(){
         Log.d(msg, "updateButtonBackgrounds() called");
-        logStateList();
-        for (int i = 0; i < DIMENSIONS; i++){
-            Button uButton = findViewById(i); //local var for updating
-            //Log.d(msg, "Button with id: " + uButton.getId() + " has state: " + mButtonStateList[i]);
-            // TODO: make try-catch block obsolete by making sure the buttons are never NULL
-            try{
+        // Check if the first button exists
+        if(findViewById(0) != null){
+            for (int i = 0; i < DIMENSIONS; i++){
+                Button uButton = findViewById(i);
                 if(mButtonStateList[i] == 0) {
                     uButton.setBackgroundResource(R.drawable.button_selector_default);
                 }
                 else if (mButtonStateList[i] == 1) {
                     uButton.setBackgroundResource(R.drawable.button_selector_selected);
-             }
+                }
                 else if (mButtonStateList[i] == 2) {
                     uButton.setBackgroundResource(R.drawable.button_selector_completed);
                     uButton.setEnabled(false);
                 }
-            } catch (NullPointerException e) {
-                e.printStackTrace();
             }
         }
     }
